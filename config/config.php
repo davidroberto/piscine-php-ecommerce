0b6cb09351d6fee517b6134125bdc90f7deb5d9e
David Robert <contact@david-robert.fr>
2024-07-15 14:46:44 +0200

    bdd connexion

--- config/config.php
+++ config/config.php
@@ -6,3 +6,14 @@ error_reporting(E_ALL);
 
 session_set_cookie_params(3600);
 
+
+$dsn = 'mysql:host=localhost:8889;dbname=piscine_php';
+$username = 'root';
+$password = 'root';
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
